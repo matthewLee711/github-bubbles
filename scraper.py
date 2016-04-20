047723c8ed84e504d707f2439fbb83f56d11f4b0
Matthew Lee <matthew.lee711@gmail.com>
2016-04-20 00:51:02 -0500

    scraper can grab number of repositories for a language

--- scraper.py
+++ scraper.py
@@ -3,13 +3,18 @@ import urllib3
 from bs4 import BeautifulSoup
 
 http = urllib3.PoolManager()
-r= http.request('GET', 'http://github.com')
+r = http.request('GET', 'https://github.com/search?q=java')
 htmlSource = r.data
 
-soup = BeautifulSoup(htmlSource)
+soup = BeautifulSoup(htmlSource, "html.parser")
+#print(htmlSource)
+mystuff = soup.find("div", class_="sort-bar")
 
+#print(soup.h3)
+#print(mystuff.encode("utf-8"))
 
-
+for row in mystuff.find_all('h3'):
+	print(row.encode("utf-8"))
 
 # soup = BeautifulSoup(r, "html.parser")
 
